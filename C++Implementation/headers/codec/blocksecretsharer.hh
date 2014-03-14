cfff9155e098997a4432ddbffb3a9c81299337cc
Jim Sinclair <jim.sinclair@deorca.com>
2014-03-14 19:10:48 +0100

    Add copyright and GPL to all source

--- C++Implementation/headers/codec/blocksecretsharer.hh
+++ C++Implementation/headers/codec/blocksecretsharer.hh
@@ -1,10 +1,23 @@
 //
-//  blocksecretsharer.h
-//  SmcsClient.ArrayBpXorEncoding
+//  blocksecretsharer.hh
 //
 //  Created by James Sinclair on 03/02/2014.
 //  Copyright (c) 2014 James Sinclair. All rights reserved.
 //
+//  This file is part of Array-BPXOR Secret-sharer.
+//
+//    Array-BPXOR Secret-sharer is free software: you can redistribute it and/or
+//    modify it under the terms of the GNU General Public License as published
+//    by the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Array-BPXOR Secret-sharer is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Array-BPXOR Secret-sharer.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef SmcsClient_ArrayBpXorEncoding_blocksecretsharer_h
 #define SmcsClient_ArrayBpXorEncoding_blocksecretsharer_h
--- PythonImplementation/restrictedbnmatrix.py
+++ PythonImplementation/restrictedbnmatrix.py
@@ -1,3 +1,22 @@
+# restrictedbnmatrix.py
+#
+# Copyright 2014 James Sinclair
+# This file is part of Array-BPXOR Secret-sharer.
+#
+#    Array-BPXOR Secret-sharer is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Array-BPXOR Secret-sharer is distributed in the hope that it will be
+#    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Array-BPXOR Secret-sharer.  If not, 
+#    see <http://www.gnu.org/licenses/>.
+
 """Defines class RestrictedBNMatrix"""
 
 from random import seed
